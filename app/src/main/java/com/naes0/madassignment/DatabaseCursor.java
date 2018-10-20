134c612cce0b2266d72a328f1dc5db98f4e5f79a
Naes0 <seanoneill8@gmail.com>
2018-10-21 03:51:11 +0800

    finished dbcursor

--- app/src/main/java/com/naes0/madassignment/DatabaseCursor.java
+++ app/src/main/java/com/naes0/madassignment/DatabaseCursor.java
@@ -39,7 +39,7 @@ public class DatabaseCursor extends CursorWrapper
         player.setHealth(health);
         player.setEquipMass(equipMass);
         player.setEquipmentList(equipmentList);
-        String s = player.createEquipment();
+        String s = player.createEquipment(  );
         player.setEquipment(s);
         return player;
     }
